5d678fcc348c4c2cc698600ae1506ba48816e493
wesamustafa <wesamustafa@gmail.com>
2021-08-29 16:07:25 +0200

    add script use to change active link in navbar

--- js/main.js
+++ js/main.js
@@ -16,5 +16,17 @@ $(document).ready(function(){
         $("#menu").removeClass("fa-times");
         $(".navbar").removeClass("nav-toggle");
         $(".login-form").removeClass("popup");
+
+    //change class active
+    $('section').each(function(){
+        let top = $(window).scrollTop();
+        let height = $(this).height();
+        let id  = $(this).attr('id');
+        let offset = $(this).offset().top - 200;
+        if(top > offset && top < offset +height){
+            $('.navbar ul li a').removeClass('active');
+            $('.navbar').find(`[href="#${id}"]`).addClass('active');
+        }
+    })
     })
 });
